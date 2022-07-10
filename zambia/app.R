583e1a59073226dd2863ab9f047616de15438410
bensonkenduiywo <bensonkemboi@gmail.com>
2022-07-10 22:21:59 +0300

    --initial

---
+++ zambia/app.R
@@ -0,0 +1,32 @@
+library(shiny)
+library(tmap)
+Zambia <- shapefile("Zambia_Maize_Forecasts.shp")
+#Rename the variables
+names(Zambia)[2] <- "Water_Stress"
+names(Zambia)[3] <- "LAI"
+names(Zambia)[4] <- "Yield_kg_ha"
+zmb_vars <-  names(Zambia)
+
+ui <- fluidPage(
+  tmapOutput("map"),
+  selectInput("var", "Variable", zmb_vars)
+)
+
+server <- function(input, output, session) {
+  output$map <- renderTmap({
+    tm_shape(Zambia) +
+      tm_polygons(zmb_vars[1], zindex = 401)
+  })
+  
+  observe({
+    var <- input$var
+    tmapProxy("map", session, {
+      tm_remove_layer(401) +
+        tm_shape(Zambia) +
+        tm_polygons(var, zindex = 401)
+    })
+  })
+}	
+
+
+shinyApp(ui, server)
